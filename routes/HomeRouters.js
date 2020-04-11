75d1e26543b05058152ace32b020ce9481559031
dochinhthtn <dochinhthtn@gmail.com>
2020-04-11 19:44:43 +0700

    Rename all routers

--- routes/HomeRouters.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const HomeController = require('../controllers/HomeController');
-
-router.get('/', HomeController.index);
-router.get('/about', HomeController.about);
-
-module.exports = router;
