85d2e3064457f2b68fcbb9574483775070cd2a26
dochinhthtn <dochinhthtn@gmail.com>
2020-04-11 19:43:39 +0700

    Modified controllers

--- controllers/SurveyController.js
+++ controllers/SurveyController.js
@@ -2,31 +2,83 @@ const Controller = require('./Controller');
 
 const Survey = require('../models/Survey');
 const Question = require('../models/Question');
+const Answer = require('../models/Answer');
 
 class SurveyController extends Controller {
     static async viewSurveys(req, res) {
         // get data from database
-        let surveys = await Survey.selectWhere('all');
+        let surveys = await Survey.selectWhere('1 ORDER BY dateModified DESC');
 
         for(let survey of surveys){
             survey.owner = await survey.getOwner();
+            survey.voteNumber = await survey.getVoteNumber();
         }
 
         res.render('survey/index', {surveys: surveys, currentUser: req.currentUser});
     }
 
-    static viewOwnSurveys(req, res) {
+    static async viewOwnSurveys(req, res) {
+        let currentUser = req.currentUser;
 
+        if(!currentUser){
+            res.render('layouts/error');
+            return;
+        }
+
+        let surveys = await Survey.selectWhere(`ownerId = '${currentUser.id}' ORDER BY dateModified DESC`);
+        for(let survey of surveys){
+            survey.voteNumber = await survey.getVoteNumber();
+        }
+        res.render('survey/survey_management', {surveys: surveys});
     }
 
-    static viewSurveyEditor(req, res) {
-        res.render('survey/survey_editor');
+    static async viewSurveyEditor(req, res) {
+        let currentUser = req.currentUser;
+        if(!currentUser){
+            res.render('layouts/error');
+            return;
+        }
+
+        let surveyInfo = {
+            id: '',
+            title: '',
+            hashTag: '',
+            description: ''
+        };
+        let surveyQuestions = [];
+
+        let surveyId = req.params.id;
+
+        if(surveyId){
+            let currentSurvey = await Survey.find(surveyId);
+            surveyInfo.id = currentSurvey.id;
+            surveyInfo.title = currentSurvey.title;
+            surveyInfo.hashTag = currentSurvey.hashTag;
+            surveyInfo.description = currentSurvey.description;
+
+            let currentQuestions = await currentSurvey.getQuestions();
+            for(let currentQuestion of currentQuestions){
+                let jsonData = JSON.parse(currentQuestion.content);
+                let question = {
+                    id: currentQuestion.id,
+                    type: jsonData.type,
+                    content: jsonData.content,
+                    choices: jsonData.choices || null
+                };
+                surveyQuestions.push(question);
+            }
+        }
+
+        res.render('survey/survey_editor', {
+            surveyInfo: surveyInfo,
+            surveyQuestions: surveyQuestions
+        });
     }
 
     static async handleSurveyEditor(req, res) {
         let status = {
             code: 1,
-            message: 'Create survey successfully'
+            message: ''
         };
 
         let currentUser = req.currentUser;
@@ -36,35 +88,162 @@ class SurveyController extends Controller {
         
         // save survey info
         let surveyInfo = surveyData.surveyInfo;
-        let newSurvey = await Survey.create({
-            title: surveyInfo.title,
-            hashTag: surveyInfo.hashTag,
-            description: surveyInfo.description,
-            ownerId: currentUser.id
-        });
+        let surveyId = surveyInfo.id;
+        if(surveyId){
+            let currentSurvey = await Survey.find(surveyId);
+            currentSurvey.title = surveyInfo.title;
+            currentSurvey.hashTag = surveyInfo.hashTag;
+            currentSurvey.description = surveyInfo.description;
+            await currentSurvey.save();
+        } else {
+            let newSurvey = await Survey.create({
+                title: surveyInfo.title,
+                hashTag: surveyInfo.hashTag,
+                description: surveyInfo.description,
+                ownerId: currentUser.id
+            });
+            surveyId = newSurvey.id;
+        }
+
 
         // save survey questions
         let surveyQuestions = surveyData.surveyQuestions;
-        // nếu người dùng ko thêm câu hỏi ở survey editor --> surveyData không chứa surveyQuestions
-        // undefined ~ false
         if(surveyQuestions) {
             for(let question of surveyQuestions){
+                let questionId = question.id;
+
+                delete question.id;
                 let questionContent = JSON.stringify(question);
-                await Question.create({
-                    content: questionContent,
-                    surveyId: newSurvey.id
-                });
+
+                if(questionId){
+                    let currentQuestion = await Question.find(questionId);
+                    currentQuestion.content = questionContent;
+                    await currentQuestion.save();
+                } else {
+                    await Question.create({
+                        content: questionContent,
+                        surveyId: surveyId
+                    });
+                }
             }
         }
 
         res.json(status);
     }
 
-    static answerSurvey(req, res) {
+    static async answerSurvey(req, res) {
+        let currentUser = req.currentUser;
+        if(!currentUser){
+            res.render('layouts/error');
+            return;
+        }
 
+        let surveyId = req.params.id;
+        let currentSurvey = await Survey.find(surveyId);
+        currentSurvey.owner = await currentSurvey.getOwner();
+        currentSurvey.voteNumber = await currentSurvey.getVoteNumber();
+        let questionsRaw = await currentSurvey.getQuestions();
+        let questions = [];
+        for(let questionRaw of questionsRaw){
+            let answerRaw = await questionRaw.getUserAnswer(currentUser.id);
+            let answer = (answerRaw) ? answerRaw.content : '';
+            let question = {
+                id: questionRaw.id,
+                data: JSON.parse(questionRaw.content),
+                answer: answer
+            };
+            questions.push(question);
+        }
+
+
+        res.render('survey/survey_answer_sheet', {survey: currentSurvey, questions: questions});
     }
 
-    static submitAnswers(req, res) {
+    static async submitAnswers(req, res) {
+        let currentUser = req.currentUser;
+        let surveyId = req.params.id;
+        if(!currentUser || !surveyId) {
+            res.render('layouts/error');
+            return;
+        }
+
+        let answers = req.body;
+        for(let key in answers){
+            let questionId = key.split('-')[1];
+            
+            let answerContent = answers[key];
+            let existAnswers = await Answer.selectWhere(`questionId = '${questionId}' AND ownerId = '${currentUser.id}'`);
+            if(existAnswers.length > 0){
+                let existAnswer = existAnswers[0];
+                existAnswer.content = answerContent;
+                await existAnswer.save();
+            } else {
+                await Answer.create({
+                    questionId: questionId,
+                    ownerId: currentUser.id,
+                    content: answerContent
+                });
+            }
+        }
+
+        res.json({
+            code: 1,
+            message: 'Save answer successfully'
+        });
+    }
+
+    static async showResult(req, res){
+        let surveyId = req.params.id;
+        if(!surveyId){
+            res.render('layouts/error');
+            return;
+        }
+        // get current survey
+        let currentSurvey = await Survey.find(surveyId);
+        currentSurvey.owner = await currentSurvey.getOwner();
+        currentSurvey.voteNumber = await currentSurvey.getVoteNumber();
+
+        // get all questions in raw
+        let questions = [];
+        let questionsRaw = await currentSurvey.getQuestions();
+
+        for(let questionRaw of questionsRaw){
+            
+            let data = JSON.parse(questionRaw.content);
+            let question = {
+                id: questionRaw.id,
+                type: data.type,
+                content: data.content,
+                choices: data.choices || null,
+                answers: []
+            };
+
+            // get all answers
+            let answersRaw = await questionRaw.getAllAnswers();
+            if(question.type == 'Text Question'){
+                question.answers = answersRaw;
+            } else {
+                question.answers = question.choices.map(() => 0);
+                for(let answerRaw of answersRaw){
+                    answerRaw.content.split(',').forEach(function(index){
+                        index = Number(index);
+                        if(question.choices[index]){
+                            if(!question.answers[index]) {
+                                question.answers[index] = 1;
+                            } else {
+                                question.answers[index] += 1;
+                            }
+                        }
+                    });
+                }
+            }
+
+            questions.push(question);
+        }
+
+        // console.log(questions);
+
+        res.render('survey/survey_result', {survey: currentSurvey, questions: questions});
 
     }
 
