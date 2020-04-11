0a965bc4b8a2cef09fd45949e47b0d057451df55
dandannghe <dandanghe2000@gmail.com>
2020-04-11 20:58:40 +0700

    add surveyAnswerSheetHandler.js

--- public/js/handlers/reportHandler.js
+++ public/js/handlers/reportHandler.js
@@ -1,3 +1,23 @@
 class ReportHandler{
-    
+    static async sendReport(event){
+        event.preventDefault();
+        let rules = {
+            'surveyId': 'required',
+            'message': 'required'
+        };
+
+        let messages = {
+            'message.required': 'Input your report message.'
+        };
+        let validator = Validator.validateForm('#form-report', rules, messages);
+        if(validator.isPassed()){
+            let result = await $.ajax({
+                url: '/report',
+                type: 'POST',
+                data: validator.data
+            });
+        } else {
+            validator.showErrors();
+        }
+    }
 }
--- public/js/handlers/surveyEditorHandler.js
+++ public/js/handlers/surveyEditorHandler.js
@@ -1,65 +1,79 @@
-class SurveyEditorHandler{
-    static addQuestion(questionType){
-        
-        let html = '';
-        if(questionType == 'Single-Choice Question' || questionType == 'Multiple-Choice Question'){
-            html = `
-            <div class="question">
-                <input type="hidden" class="question-type" value="${questionType}">
-                <input type="hidden" class="question-id" value="">
+class SurveyEditorHandler {
 
-                <div class="question-buttons float-right">
-                    <button class="btn btn-success btn-sm" onclick="SurveyEditorHandler.addChoice(this)"><i class="fa fa-plus" aria-hidden="true"></i> Add choice</button>
-                    <button class="btn btn-danger btn-sm" onclick="SurveyEditorHandler.deleteQuestion(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>
-                </div>
-                <div class="form-group">
-                    <label>${questionType}:</label>
-                    <textarea cols="30" rows="5" class="content form-control"></textarea>
-                </div>
-                <div class="question-choices">
-                    <label>Choices: </label>
+    static addQuestion(type, data = {}) {
+        // let data = JSON.parse(jsonData);
+        let id = data.id || '';
+        let content = data.content || '';
 
+        let question = null;
+        if (type == 'Single-Choice Question' || type == 'Multiple-Choice Question') {
+            let choices = data.choices || [''];
+            question = $(`
+                <div class="question">
+                    <input type="hidden" class="question-type" value="${type}">
+                    <input type="hidden" class="question-id" value="${id}">
+
+                    <div class="question-buttons float-right">
+                        <button class="btn btn-success btn-sm" onclick="SurveyEditorHandler.addChoice(this)"><i class="fa fa-plus" aria-hidden="true"></i> Add choice</button>
+                        <button class="btn btn-danger btn-sm" onclick="SurveyEditorHandler.deleteQuestion(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                    </div>
+                    <div class="form-group">
+                        <label>${type}:</label>
+                        <textarea cols="30" rows="5" class="content form-control">${content}</textarea>
+                    </div>
+                    <div class="question-choices">
+                        <label>Choices: </label>
+                    </div>
+                </div>
+             `);
+
+            for (let choice of choices) {
+                $(question).find('.question-choices').append(`
                     <div class="input-group mb-3">
-                        <input type="text" class="choice form-control" placeholder="Input choice content">
+                        <input type="text" class="choice form-control" placeholder="Input choice content" value="${choice}">
                         <div class="input-group-append">
                             <button class="btn btn-danger" onclick="SurveyEditorHandler.deleteChoice(this)"><i class="fa fa-times" aria-hidden="true"></i></button>
                         </div>
                     </div>
-                    
-                </div>
-            </div>
-            `;
-        } else if(questionType == 'Text Question') {
-            html = `
-            <div class="question">
+                `);
+            }
 
-                <input type="hidden" class="question-type" value="${questionType}">
-                <input type="hidden" class="question-id" value="">
-
-                <div class="question-buttons float-right">
-                    <button class="btn btn-danger btn-sm" onclick="SurveyEditorHandler.deleteQuestion(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>
-                </div>
-
-                <div class="form-group">
-                    <label>Text Question:</label>
-                    <textarea cols="30" rows="5" class="content form-control"></textarea>
-                </div>
-
-            </div>
-            `;
+        } else if (type == 'Text Question') {
+            question = $(`
+                <div class="question">
+                    <input type="hidden" class="question-type" value="Text Question">
+                    <input type="hidden" class="question-id" value="${id}">
+                    <div class="question-buttons float-right">
+                        <button class="btn btn-danger btn-sm" onclick="SurveyEditorHandler.deleteQuestion(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                    </div>
+                    <div class="form-group">
+                        <label>Text Question:</label>
+                        <textarea cols="30" rows="5" class="content form-control">${content}</textarea>
+                    </div>
+                </div>        
+            `);
         }
 
-
-        $(".questions-container").append(html);
+        $(".questions-container").append(question);
 
     }
 
-    static deleteQuestion(button){
+    static deleteQuestion(button) {
         // get parent .question of button and remove 
-        $(button).parents(".question").remove();
+        let confirmDelete = confirm("Are you sure to delete this question?");
+        if(confirmDelete){
+            let questionId = $(button).parents('.question').find('.question-id').val();
+            if(questionId){
+                $.ajax({
+                    url: '/question/delete/' + questionId,
+                    type: 'GET',
+                });
+            }
+            $(button).parents(".question").remove();
+        }
     }
 
-    static addChoice(button){
+    static addChoice(button) {
         let html = `
         <div class="input-group mb-3">
             <input type="text" class="choice form-control" placeholder="Input choice content">
@@ -72,14 +86,15 @@ class SurveyEditorHandler{
         $(button).parents(".question").find('.question-choices').append(html);
     }
 
-    static deleteChoice(button){
+    static deleteChoice(button) {
         $(button).parents(".input-group").remove();
     }
 
-    static async saveSurvey(event){
+    static async saveSurvey(event) {
         event.preventDefault();
 
         let rules = {
+            'id': 'nullable',
             'title': 'required',
             'hashTag': 'required',
             'description': 'required'
@@ -92,8 +107,8 @@ class SurveyEditorHandler{
         };
 
         let validator = Validator.validateForm('.form-edit-survey', rules, messages);
-        
-        if(validator.isPassed()){
+
+        if (validator.isPassed()) {
             // lấy ra câu hỏi người dùng chỉnh sửa survey questions
             let questionsData = SurveyEditorHandler.getQuestionsData();
 
@@ -107,7 +122,7 @@ class SurveyEditorHandler{
                 }
             });
 
-            if(result.code == 1){
+            if (result.code == 1) {
                 load('/survey');
             } else {
                 console.log('error');
@@ -119,7 +134,7 @@ class SurveyEditorHandler{
         }
     }
 
-    static getQuestionsData(){
+    static getQuestionsData() {
 
         /* 
             id
@@ -131,17 +146,17 @@ class SurveyEditorHandler{
         let questionsData = [];
 
         let questions = $('.question');
-        for(let question of questions){
+        for (let question of questions) {
             let questionData = {
                 id: $(question).find('.question-id').val(),
                 type: $(question).find('.question-type').val(),
                 content: $(question).find('.content').val()
             };
 
-            if(questionData.type != 'Text Question'){
+            if (questionData.type != 'Text Question') {
                 questionData.choices = [];
                 let choices = $(question).find('.choice');
-                for(let choice of choices){
+                for (let choice of choices) {
                     questionData.choices.push($(choice).val());
                 }
             }
@@ -153,3 +168,66 @@ class SurveyEditorHandler{
         return questionsData;
     }
 }
+
+
+
+// static addChoiceQuestion(type, jsonData = {}) {
+//     let data = JSON.parse(jsonData);
+//     let id = data.id || '';
+//     let content = data.content || '';
+//     let choices = data.choices || [''];
+
+//     let question = $(`
+//         <div class="question">
+//             <input type="hidden" class="question-type" value="${type}">
+//             <input type="hidden" class="question-id" value="${id}">
+
+//             <div class="question-buttons float-right">
+//                 <button class="btn btn-success btn-sm" onclick="SurveyEditorHandler.addChoice(this)"><i class="fa fa-plus" aria-hidden="true"></i> Add choice</button>
+//                 <button class="btn btn-danger btn-sm" onclick="SurveyEditorHandler.deleteQuestion(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>
+//             </div>
+//             <div class="form-group">
+//                 <label>${type}:</label>
+//                 <textarea cols="30" rows="5" class="content form-control">${content}</textarea>
+//             </div>
+//             <div class="question-choices">
+//                 <label>Choices: </label>
+//             </div>
+//         </div>
+//     `);
+
+//     for (let choice of choices) {
+//         $(question).find('.question-choices').append(`
+//             <div class="input-group mb-3">
+//                 <input type="text" class="choice form-control" placeholder="Input choice content" value="${choice}">
+//                 <div class="input-group-append">
+//                     <button class="btn btn-danger" onclick="SurveyEditorHandler.deleteChoice(this)"><i class="fa fa-times" aria-hidden="true"></i></button>
+//                 </div>
+//             </div>
+//         `);
+//     }
+
+//     $(".questions-container").append(html);
+
+// }
+
+
+
+// static addTextQuestion(jsonData = {}) {
+
+
+//     let question = $(`
+//         <div class="question">
+//             <input type="hidden" class="question-type" value="Text Question">
+//             <input type="hidden" class="question-id" value="${id}">
+//             <div class="question-buttons float-right">
+//                 <button class="btn btn-danger btn-sm" onclick="SurveyEditorHandler.deleteQuestion(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>
+//             </div>
+//             <div class="form-group">
+//                 <label>Text Question:</label>
+//                 <textarea cols="30" rows="5" class="content form-control">${content}</textarea>
+//             </div>
+//         </div>        
+//     `);
+
+// }
