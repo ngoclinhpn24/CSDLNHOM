7ba7e3f3fbcf87eee0e70d38a282bc4497c9bb16
dandannghe <dandanghe2000@gmail.com>
2020-04-11 21:01:57 +0700

    Rename all routers

---
+++ routes/SurveyRouter.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const SurveyController = require('../controllers/SurveyController');
+
+router.get('/', SurveyController.viewSurveys);
+router.get('/management', SurveyController.viewOwnSurveys);
+
+router.get('/editor/:id?', SurveyController.viewSurveyEditor);
+router.post('/editor/:id?', SurveyController.handleSurveyEditor);
+
+router.get('/answer/:id', SurveyController.answerSurvey)
+router.post('/answer/:id?', SurveyController.submitAnswers);
+
+router.get('/result/:id', SurveyController.showResult);
+
+router.delete('/delete/:id', SurveyController.deleteSurvey);
+
+module.exports = router;
